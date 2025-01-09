17d88197d446cfc1bccf448944a3cb72b9a6ef07
Longle290204 <longvis2004@gmail.com>
2025-01-09 18:04:53 +0700

    add: List product Page

---
+++ src/pages/AdminPage/ListProductPage/ListProductPage.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import ProductList from '../../../components/Products/ProductList/ProductList';
+import axios from 'axios';
+import classNames from 'classnames/bind';
+import styles from './ListProductPage.module.scss';
+
+const cx = classNames.bind(styles);
+
+function ListProductPage() {
+   const [products, setProducts] = useState([]);
+   useEffect(() => {
+      const axiosProducts = async () => {
+         const response = await axios.get('http://localhost:3002/products');
+         setProducts(response.data);
+      };
+
+      axiosProducts();
+   });
+
+   // Delete Product
+   const handleDeleteProduct = (productId) => {
+      setProducts((prevProducts) => prevProducts.filter((product) => product.id !== productId));
+   };
+
+   //  Update Product
+   const handleUpdateProduct = (updateProduct) => {
+      setProducts((prevProducts) => prevProducts.map((product) => (product.id === updateProduct.id ? updateProduct : product)));
+   };
+   return (
+      <div>
+         <ProductList products={products} onDelete={handleDeleteProduct} onUpdate={handleUpdateProduct} />
+         {/* <Admin /> */}
+      </div>
+   );
+}
+
+export default ListProductPage;
