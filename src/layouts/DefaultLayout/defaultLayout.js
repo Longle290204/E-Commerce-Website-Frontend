b6d6dbee2da0b8f3912dd3e411aed78dd1783310
Longle290204 <longvis2004@gmail.com>
2025-01-09 15:10:34 +0700

    default  layout

--- src/layouts/DefaultLayout/defaultLayout.js
+++ src/layouts/DefaultLayout/defaultLayout.js
@@ -2,18 +2,27 @@ import Header from "../components/Header/header";
 import Footer from "../components/Footer/footer";
 import classNames from "classnames/bind";
 import styles from "./DefaultLayout.module.scss";
+import PropTypes from "prop-types";
+import Navbar from "../components/Header/Header-nav/navbar";
 
-function DefaultLayout() {
-    const cx = classNames.bind(styles);
-    return (
-        <div className={cx("wrapper")}>
-            <Header />
-            <div className={cx("content")}></div>
-            <div className={cx("container")}>
-                <Footer />
-            </div>
+function DefaultLayout({ children }) {
+  const cx = classNames.bind(styles);
+
+  return (
+    <div className={cx("wrapper")}>
+      <Header />
+      <div className={cx("container")}>
+        <div className={cx("content")}>
+          <Navbar>{children}</Navbar>
         </div>
-    );
+        <Footer />
+      </div>
+    </div>
+  );
 }
+
+DefaultLayout.prototypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default DefaultLayout;
