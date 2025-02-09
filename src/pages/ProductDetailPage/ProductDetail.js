ccdd2b91b6778b225a3b28a5487a1910f48c221c
Longle290204 <longvis2004@gmail.com>
2025-02-09 17:05:22 +0700

    style: Add product thumbnail interface

---
+++ src/pages/ProductDetailPage/ProductDetail.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Card, CardMedia, Container } from '@mui/material';
+import Grid from '@mui/material/Grid2';
+import ProductThumbnail from './ProductThumbnail';
+import ProductInfo from './ProductInfo';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import hunterwhite2 from '../../assets/images/hunterwhite2.webp';
+
+const ProductPage = () => {
+   const theme = createTheme({
+      typography: {
+         h5: {
+            display: 'flex',
+            fontSize: '1.4rem',
+            alignItems: 'center',
+            marginBottom: '10px',
+            gap: '5px',
+         },
+      },
+
+      breakpoints: {
+         values: {
+            xs: 0,
+            sm: 600,
+            md: 900,
+            lg: 1350,
+            xl: 1536,
+         },
+      },
+
+      components: {
+         MuiContainer: {
+            defaultProps: {
+               maxWidth: 'xl', // Mặc định là xl
+            },
+            styleOverrides: {
+               maxWidthXl: {
+                  maxWidth: '1350px !important', // Khi maxWidth="xl", nó sẽ dùng 1400px
+               },
+            },
+         },
+      },
+   });
+
+   return (
+      <ThemeProvider theme={theme}>
+         <Container maxWidth="xl" disableGutters>
+            <Grid container spacing={5} sx={{ padding: 2 }}>
+               {/* Cột bên trái */}
+               <Grid item xl={6}>
+                  <ProductThumbnail />
+               </Grid>
+            </Grid>
+         </Container>
+      </ThemeProvider>
+   );
+};
+
+export default ProductPage;
--- src/pages/ProductDetailPage/ProductThumbnail.js
+++ src/pages/ProductDetailPage/ProductThumbnail.js
@@ -9,11 +9,13 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation, Scrollbar, EffectFade, Thumbs, FreeMode } from 'swiper/modules';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import hunterwhite1 from '../../assets/images/hunterwhite1.webp';
 import hunterwhite2 from '../../assets/images/hunterwhite2.webp';
 import hunterwhite3 from '../../assets/images/hunterwhite3.webp';
 import hunterwhite4 from '../../assets/images/hunterwhite4.webp';
 import hunterwhite5 from '../../assets/images/hunterwhite5.jpg';
+import { color } from '@mui/system';
 
 const cx = classNames.bind(styles);
 
@@ -24,83 +26,110 @@ function ProductThumbnail() {
 
    const images = [hunterwhite1, hunterwhite2, hunterwhite3, hunterwhite4, hunterwhite5];
 
+   const theme = createTheme({
+      components: {
+         MuiIconButton: {
+            styleOverrides: {
+               root: {
+                  variants: [
+                     {
+                        props: { color: 'primary' },
+                        style: {
+                           color: '#000',
+                           backgroundColor: '#fff',
+                           ':hover': {
+                              backgroundColor: '#fff',
+                           },
+                        },
+                     },
+                  ],
+               },
+            },
+         },
+      },
+   });
+
    return (
-      <Box sx={{ display: 'flex', gap: '10px' }}>
-         {/* =================== THUMBNAIL BEN TRÁI ==================== */}
-         <Swiper
-            spaceBetween={10}
-            slidesPerView={4}
-            direction="vertical"
-            loop={true}
-            observer={true} // Cập nhật Swiper khi component thay đổi
-            observeParents={true} // Cập nhật Swiper nếu container thay đổi
-            watchSlidesProgress={true}
-            watchSlidesVisibility={true} // 🔹 Giúp Swiper nhận diện slide nào đang hiển thị
-            modules={[Scrollbar, FreeMode]}
-            freeMode={{ enabled: false, momentumBounce: false }} // Giảm hiện tượng đứng nửa vời
-            speed={500}
-            onSwiper={setThumbsSwiper}
-            className={cx('thumbnail-slider')}
-         >
-            {images.map((src, index) => (
-               <SwiperSlide key={index}>
-                  <img src={src} alt={`Thumbnail ${index + 1}`} />
-               </SwiperSlide>
-            ))}
-         </Swiper>
-
-         {/* =================== SLIDE CHÍNH ==================== */}
-         <Swiper
-            onSwiper={(swiper) => (swiperRef.current = swiper)}
-            ref={swiperRef} // Gán ref vào Swiper
-            modules={[Navigation, Thumbs, EffectFade]}
-            // navigation={{ nextEl: '.next-btn', prevEl: '.prev-btn' }}
-            // navigation={true}
-            observer={true} // Cập nhật Swiper khi component thay đổi
-            observeParents={true}
-            loop={true}
-            thumbs={{ swiper: thumbsSwiper }}
-            speed={500}
-            watchOverflow={true}
-            allowTouchMove={true} // Đảm bảo có thể kéo
-            resistanceRatio={0} // Giảm lực cản khi kéo
-            onSlideChange={(swiper) => {
-               const activeIndex = swiper.realIndex; // 🔹 Dùng realIndex để đồng bộ
-               thumbsSwiper?.slideToLoop(activeIndex); // 🔹 Dùng slideToLoop để giữ đồng bộ
-            }}
-            className={cx('main-slider')}
-         >
-            {images.map((src, index) => (
-               <SwiperSlide key={index}>
-                  <img src={src} alt={`Product ${index + 1}`} className={cx('main-image')} />
-               </SwiperSlide>
-            ))}
-
-            {/* =================== NÚT ĐIỀU HƯỚNG ==================== */}
-            <IconButton
-               onClick={() => {
-                  swiperRef.current?.slidePrev();
-                  const activeIndex = swiperRef.current?.realIndex;
-                  thumbsSwiper?.slideToLoop(activeIndex);
-               }}
-               sx={{ position: 'absolute', transform: 'translateY(-50%)' }}
-               className={cx('prev-btn')}
+      <ThemeProvider theme={theme}>
+         <Box sx={{ display: 'flex', gap: '10px' }}>
+            {/* =================== THUMBNAIL BEN TRÁI ==================== */}
+            <Swiper
+               spaceBetween={10}
+               slidesPerView={4}
+               direction="vertical"
+               loop={true}
+               observer={true} // Cập nhật Swiper khi component thay đổi
+               observeParents={true} // Cập nhật Swiper nếu container thay đổi
+               watchSlidesProgress={true}
+               watchSlidesVisibility={true} // 🔹 Giúp Swiper nhận diện slide nào đang hiển thị
+               modules={[Scrollbar, FreeMode]}
+               freeMode={{ enabled: false, momentumBounce: false }} // Giảm hiện tượng đứng nửa vời
+               speed={500}
+               onSwiper={setThumbsSwiper}
+               className={cx('thumbnail-slider')}
             >
-               <ArrowBackIosNewIcon />
-            </IconButton>
-            <IconButton
-               onClick={() => {
-                  swiperRef.current?.slideNext();
-                  const activeIndex = swiperRef.current?.realIndex;
-                  thumbsSwiper?.slideToLoop(activeIndex);
+               {images.map((src, index) => (
+                  <SwiperSlide key={index}>
+                     <img src={src} alt={`Thumbnail ${index + 1}`} />
+                  </SwiperSlide>
+               ))}
+            </Swiper>
+
+            {/* =================== SLIDE CHÍNH ==================== */}
+            <Swiper
+               onSwiper={(swiper) => (swiperRef.current = swiper)}
+               ref={swiperRef} // Gán ref vào Swiper
+               modules={[Navigation, Thumbs, EffectFade]}
+               // navigation={{ nextEl: '.next-btn', prevEl: '.prev-btn' }}
+               // navigation={true}
+               observer={true} // Cập nhật Swiper khi component thay đổi
+               observeParents={true}
+               loop={true}
+               thumbs={{ swiper: thumbsSwiper }}
+               speed={500}
+               watchOverflow={true}
+               allowTouchMove={true} // Đảm bảo có thể kéo
+               resistanceRatio={0} // Giảm lực cản khi kéo
+               onSlideChange={(swiper) => {
+                  const activeIndex = swiper.realIndex; // 🔹 Dùng realIndex để đồng bộ
+                  thumbsSwiper?.slideToLoop(activeIndex); // 🔹 Dùng slideToLoop để giữ đồng bộ
                }}
-               sx={{ position: 'absolute', transform: 'translateY(-50%)' }}
-               className={cx('next-btn')}
+               className={cx('main-slider')}
             >
-               <ArrowForwardIosIcon />
-            </IconButton>
-         </Swiper>
-      </Box>
+               {images.map((src, index) => (
+                  <SwiperSlide key={index}>
+                     <img src={src} alt={`Product ${index + 1}`} className={cx('main-image')} />
+                  </SwiperSlide>
+               ))}
+
+               {/* =================== NÚT ĐIỀU HƯỚNG ==================== */}
+               <IconButton
+                  color="primary"
+                  onClick={() => {
+                     swiperRef.current?.slidePrev();
+                     const activeIndex = swiperRef.current?.realIndex;
+                     thumbsSwiper?.slideToLoop(activeIndex);
+                  }}
+                  sx={{ position: 'absolute', transform: 'translateY(-50%)' }}
+                  className={cx('prev-btn')}
+               >
+                  <ArrowBackIosNewIcon />
+               </IconButton>
+               <IconButton
+                  color="primary"
+                  onClick={() => {
+                     swiperRef.current?.slideNext();
+                     const activeIndex = swiperRef.current?.realIndex;
+                     thumbsSwiper?.slideToLoop(activeIndex);
+                  }}
+                  sx={{ position: 'absolute', transform: 'translateY(-50%)' }}
+                  className={cx('next-btn')}
+               >
+                  <ArrowForwardIosIcon />
+               </IconButton>
+            </Swiper>
+         </Box>
+      </ThemeProvider>
    );
 }
 
