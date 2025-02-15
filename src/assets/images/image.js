0641624cc8ef8a637cb68ddd4828ed960f5bb565
Longle290204 <longvis2004@gmail.com>
2025-02-15 12:57:45 +0700

    chore:

--- src/assets/images/image.js
+++ src/assets/images/image.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import logoLevion from './logoLevion.svg';
+import heart from '../svg/heart.svg';
+import plus from '../svg/plus.svg';
 
 const images = {
    logo,
    logoLevion,
+   heart,
+   plus,
 };
 
 export default images;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,29 @@ module.exports = {
    content: ['./src/**/*.{js,jsx,ts,tsx}'],
    theme: {
       extend: {
+         container: {
+            center: true,
+            padding: '2rem',
+            screens: {
+               sm: '640px',
+               md: '768px',
+               lg: '1024px',
+               xl: '1340px', // Thay đổi giá trị xl theo yêu cầu
+            },
+         },
+         
          fontSize: {
             responsive: 'clamp(1.7rem, 2vw, 1.75rem)',
          },
+
          fontFamily: {
             nunito: ['Nunito', 'sans-serif'],
             poppins: ['Poppins', 'sans-serif'],
             quicksand: ['Quicksand', 'sans-serif'],
          },
+         // fontSize: {
+         //    base: '10px', // Định nghĩa 1rem = 10px
+         // },
       },
    },
    plugins: [],
