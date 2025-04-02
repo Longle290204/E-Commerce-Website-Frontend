452a04c11080138525a5ff7df55520787ff12aa8
Longle290204 <longvis2004@gmail.com>
2025-04-03 01:54:03 +0700

    update router

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,50 +1,56 @@
-import Cart from "../pages/Cart/cart";
-import Home from "../pages/Home/home";
-import LoginForm from "../pages/Login/login";
-import NamCategory from "../pages/Category/Nam/Nam";
-import NuCategory from "../pages/Category/Nu/Nu";
-import SandalNam from "../pages/Category/Sandal/Sandal Nam/SandalNam";
-import SandalNu from "../pages/Category/Sandal/Sandal Nu/SandalNu";
-import SportNam from "../pages/Category/Sport Shoes/Sport Nam/SportNam";
-import SportNu from "../pages/Category/Sport Shoes/Sport Nu/SportNu";
-import AdminPage from "../pages/AdminPage/AdminPage";
-import ProductAdminPage from "../pages/AdminPage/ProductAdminPage/ProductAdminPage";
-import Dashboard from "../pages/AdminPage/Dashboard/Dashboard";
-import CreateProductPage from "../pages/AdminPage/CreateProductPage/CreateProductPage";
-import ListProductPage from "../pages/AdminPage/ListProductPage/ListProductPage";
-import CategoryPage from "../pages/AdminPage/CategoryPage/CategoryPages";
+import Cart from '../pages/Cart/cart';
+import Home from '../pages/Home/home';
+import LoginForm from '../pages/Login/login';
+import NamCategory from '../pages/Category/Nam/Nam';
+import NuCategory from '../pages/Category/Nu/Nu';
+import SandalNam from '../pages/Category/Sandal/Sandal Nam/SandalNam';
+import SandalNu from '../pages/Category/Sandal/Sandal Nu/SandalNu';
+import SportNam from '../pages/Category/Sport Shoes/Sport Nam/SportNam';
+import SportNu from '../pages/Category/Sport Shoes/Sport Nu/SportNu';
+import AdminPage from '../pages/AdminPage/AdminPage';
+import ProductAdminPage from '../pages/AdminPage/ProductAdminPage/ProductAdminPage';
+import Dashboard from '../pages/AdminPage/Dashboard/Dashboard';
+import CreateProductPage from '../pages/AdminPage/CreateProductPage/CreateProductPage';
+import ListProductPage from '../pages/AdminPage/ListProductPage/ListProductPage';
+import CategoryPage from '../pages/AdminPage/CategoryPage/CategoryPages';
+import CheckoutPage from '../pages/Checkout/checkout';
+import CartPage from '../pages/CartPage/CartPage';
+import ProductsNewPage from '../pages/ProductsNewPage/ProductsNewPage';
 
 const publicRoutes = [
-  { path: "/", element: Home },
-  { path: "/cart", element: Cart },
-  { path: "/login", element: LoginForm },
-  { path: "/collection/nam", element: NamCategory },
-  { path: "/collection/nu", element: NuCategory },
-  { path: "/collection/Sandal-Nam", element: SandalNam },
-  { path: "/collection/Sandal-Nu", element: SandalNu },
-  { path: "/collection/Sport-Nam", element: SportNam },
-  { path: "/collection/Sport-Nu", element: SportNu },
-  { path: "/admin", element: Dashboard, layout: AdminPage },
-  {
-    path: "/admin/products",
-    element: ProductAdminPage,
-    layout: AdminPage,
-  },
-  {
-    path: "/admin/create-product",
-    element: CreateProductPage,
-    layout: AdminPage,
-  },
-  {
-    path: "/admin/list-product",
-    element: ListProductPage,
-    layout: AdminPage,
-  },
-  {
-    path: "/admin/category",
-    element: CategoryPage,
-    layout: AdminPage,
-  },
+   { path: '/', element: Home },
+   { path: '/cart', element: Cart },
+   { path: '/login', element: LoginForm },
+   { path: '/collection/nam', element: NamCategory },
+   { path: '/collection/nu', element: NuCategory },
+   { path: '/collection/Sandal-Nam', element: SandalNam },
+   { path: '/collection/Sandal-Nu', element: SandalNu },
+   { path: '/collection/Sport-Nam', element: SportNam },
+   { path: '/collection/Sport-Nu', element: SportNu },
+   { path: '/admin', element: Dashboard, layout: AdminPage },
+   { path: '/checkout', element: CheckoutPage },
+   { path: '/cartpage', element: CartPage },
+   {path: '/collection/:slug', element: ProductsNewPage},
+   {
+      path: '/admin/products',
+      element: ProductAdminPage,
+      layout: AdminPage,
+   },
+   {
+      path: '/admin/create-product',
+      element: CreateProductPage,
+      layout: AdminPage,
+   },
+   {
+      path: '/admin/list-product',
+      element: ListProductPage,
+      layout: AdminPage,
+   },
+   {
+      path: '/admin/category',
+      element: CategoryPage,
+      layout: AdminPage,
+   },
 ];
 
 export { publicRoutes };
