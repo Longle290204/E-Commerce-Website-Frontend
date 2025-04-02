5fc8820ea1d228cb006902ea47803a80ab2e3cb6
Longle290204 <longvis2004@gmail.com>
2025-04-02 14:14:44 +0700

    fix: fix scale effect mini-cart

--- src/layouts/components/Header/Header-nav/navbar.js
+++ src/layouts/components/Header/Header-nav/navbar.js
@@ -5,6 +5,9 @@ import styles from './Navbar.module.scss';
 import { Link } from 'react-router-dom';
 import Image from '../../../images/image';
 import MiniCart from './Mini-cart/MiniCart';
+import Search from '../../../../components/function/Search/Search';
+import Tippy from '@tippyjs/react/headless';
+import { Wrapper as PopperWrapper } from '../../../components/Popper';
 
 const cx = classNames.bind(styles);
 
@@ -47,7 +50,7 @@ function Navbar({ children }) {
    }, [lastScrollTop]);
 
    return (
-      <div className={cx('wrapper', "w-screen")}>
+      <div className={cx('wrapper', 'w-screen')}>
          <div
             className={cx('header-nav', {
                'header-nav-up': isVisible,
@@ -70,125 +73,129 @@ function Navbar({ children }) {
                   </Link>
                </logo>
 
-               <div className={cx('header-wrap-menu')}>
-                  <div className={cx('navbar-menu')}>
-                     <ul className={cx('menu-list')}>
-                        {/* Nam */}
-                        <li className={cx('dropdown')}>
-                           <Link to="/collection/nam" className={cx('menu-list-title')}>
-                              NAM
-                           </Link>
-                           <ul className={cx('dropdown-content')}>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Giày thể thao nam</Link>
-                              </li>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Sandal</Link>
-                              </li>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Dép</Link>
-                              </li>
-                           </ul>
-                        </li>
-                        {/* Nữ */}
-                        <li className={cx('dropdown')}>
-                           <Link to="/collection/nu" className={cx('menu-list-title')}>
-                              NỮ
-                           </Link>
-                           <ul className={cx('dropdown-content')}>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Giày Thể Thao</Link>
-                              </li>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Sandal</Link>
-                              </li>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Giày Cao Gót</Link>
-                              </li>
-                           </ul>
-                        </li>
-                        <li className={cx('dropdown')}>
-                           <Link to="/" className={cx('menu-list-title')}>
-                              BÉ TRAI
-                           </Link>
-                           <ul className={cx('dropdown-content')}>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Giày Thể Thao</Link>
-                              </li>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Sandal</Link>
-                              </li>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Dép Bé Trai</Link>
-                              </li>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Giày tập đi</Link>
-                              </li>
-                           </ul>
-                        </li>
-                        <li className={cx('dropdown')}>
-                           <Link to="/" className={cx('menu-list-title')}>
-                              BÉ GÁI
-                           </Link>
-                           <ul className={cx('dropdown-content')}>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Giày Thể Thao</Link>
-                              </li>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Sandal</Link>
-                              </li>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Dép Bé Gái</Link>
-                              </li>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Giày Tập Đi</Link>
-                              </li>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Giày Búp Bê</Link>
-                              </li>
-                           </ul>
-                        </li>
-                        <li className={cx('dropdown')}>
-                           <Link to="/" className={cx('menu-list-title')}>
-                              PHỤ KIỆN
-                           </Link>
-                           <ul className={cx('dropdown-content')}>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Balo</Link>
-                              </li>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Balo Trẻ Em</Link>
-                              </li>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Túi Xách</Link>
-                              </li>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Ví</Link>
-                              </li>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Vớ Người Lớn</Link>
-                              </li>
-                              <li>
-                                 <Link className={cx('menu-list-subtitle')}>Vớ Trẻ Em</Link>
-                              </li>
-                           </ul>
-                        </li>
-                        <li className={cx('dropdown')}>
-                           <Link to="/" className={cx('menu-list-title')}>
-                              MỚI & RESTOCK
-                           </Link>
-                        </li>
-                        <li className={cx('dropdown')}>
-                           <Link to="/" className={cx('menu-list-title')}>
-                              GIỚI THIỆU
-                           </Link>
-                        </li>
-                        <li className={cx('dropdown')}>
-                           <Link to="/" className={cx('menu-list-title')}>
-                              TIN TỨC
-                           </Link>
-                        </li>
-                     </ul>
+               <div className="flex flex-col items-center justify-between w-full">
+                  <Search />
+
+                  <div className={cx('header-wrap-menu')}>
+                     <div className={cx('navbar-menu')}>
+                        <ul className={cx('menu-list')}>
+                           {/* Nam */}
+                           <li className={cx('dropdown')}>
+                              <Link to="/collection/nam" className={cx('menu-list-title')}>
+                                 NAM
+                              </Link>
+                              <ul className={cx('dropdown-content')}>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Giày thể thao nam</Link>
+                                 </li>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Sandal</Link>
+                                 </li>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Dép</Link>
+                                 </li>
+                              </ul>
+                           </li>
+                           {/* Nữ */}
+                           <li className={cx('dropdown')}>
+                              <Link to="/collection/nu" className={cx('menu-list-title')}>
+                                 NỮ
+                              </Link>
+                              <ul className={cx('dropdown-content')}>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Giày Thể Thao</Link>
+                                 </li>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Sandal</Link>
+                                 </li>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Giày Cao Gót</Link>
+                                 </li>
+                              </ul>
+                           </li>
+                           <li className={cx('dropdown')}>
+                              <Link to="/" className={cx('menu-list-title')}>
+                                 BÉ TRAI
+                              </Link>
+                              <ul className={cx('dropdown-content')}>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Giày Thể Thao</Link>
+                                 </li>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Sandal</Link>
+                                 </li>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Dép Bé Trai</Link>
+                                 </li>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Giày tập đi</Link>
+                                 </li>
+                              </ul>
+                           </li>
+                           <li className={cx('dropdown')}>
+                              <Link to="/" className={cx('menu-list-title')}>
+                                 BÉ GÁI
+                              </Link>
+                              <ul className={cx('dropdown-content')}>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Giày Thể Thao</Link>
+                                 </li>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Sandal</Link>
+                                 </li>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Dép Bé Gái</Link>
+                                 </li>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Giày Tập Đi</Link>
+                                 </li>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Giày Búp Bê</Link>
+                                 </li>
+                              </ul>
+                           </li>
+                           <li className={cx('dropdown')}>
+                              <Link to="/" className={cx('menu-list-title')}>
+                                 PHỤ KIỆN
+                              </Link>
+                              <ul className={cx('dropdown-content')}>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Balo</Link>
+                                 </li>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Balo Trẻ Em</Link>
+                                 </li>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Túi Xách</Link>
+                                 </li>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Ví</Link>
+                                 </li>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Vớ Người Lớn</Link>
+                                 </li>
+                                 <li>
+                                    <Link className={cx('menu-list-subtitle')}>Vớ Trẻ Em</Link>
+                                 </li>
+                              </ul>
+                           </li>
+                           <li className={cx('dropdown')}>
+                              <Link to="/" className={cx('menu-list-title')}>
+                                 MỚI & RESTOCK
+                              </Link>
+                           </li>
+                           <li className={cx('dropdown')}>
+                              <Link to="/" className={cx('menu-list-title')}>
+                                 GIỚI THIỆU
+                              </Link>
+                           </li>
+                           <li className={cx('dropdown')}>
+                              <Link to="/" className={cx('menu-list-title')}>
+                                 TIN TỨC
+                              </Link>
+                           </li>
+                        </ul>
+                     </div>
                   </div>
                </div>
 
@@ -197,11 +204,14 @@ function Navbar({ children }) {
                      <Image iconName={'profileIcon'} />
                   </div>
 
+                  <div>
+                     <Image iconName={'heartIcon'} />
+                  </div>
+
                   {/* <---- Cart btn ----> */}
+
                   <div className={cx('cart-btn')}>
                      <Image iconName={'shoppingCartIcon'} />
-
-                     {/* <---- Cart preview ----> */}
                      <div className={cx('cart-preview')}>
                         <MiniCart />
                      </div>
