8c15cc26e793593ce0de07f4a6732ae3afe08777
Longle290204 <longvis2004@gmail.com>
2025-04-05 17:28:13 +0700

    add image icon youtube, fb, insta,...v.v

--- src/assets/images/image.js
+++ src/assets/images/image.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import logoLevion from './logoLevion.svg';
 import heart from '../svg/heart.svg';
 import plus from '../svg/plus.svg';
+import twiter from '../svg/twiter.svg';
+import youtube from '../svg/youtube.svg';
 
 const images = {
    logo,
    logoLevion,
    heart,
    plus,
+   twiter,
+   youtube,
 };
 
 export default images;
